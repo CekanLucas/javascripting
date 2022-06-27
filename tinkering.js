fdc44e5707a7b9ad6a9b04bbd40157a31ef506c6
CekanLucas <cekanlucas@gmail.coms>
2022-06-27 14:40:18 +0000

    added tinkering explanation

--- tinkering.js
+++ tinkering.js
@@ -1,8 +1,13 @@
+/**
+ * This is a node assignment 
+ * node this file to run this file
+*/
+
 function multiply(a,b) {
 	return a * b;
 }
 
 const result = multiply(2,4);
 
-console.log(result);
+console.log(result); //=> 8
 
